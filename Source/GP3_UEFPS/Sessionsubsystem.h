72385e7b895bc54d619a777ea0255ae40fd1a3c9
Yuto1008 <ktc23a31a0032@edu.kyoto-tech.ac.jp>
2025-10-09 16:24:37 +0900

    SessionTestを追加

---
+++ Source/GP3_UEFPS/Sessionsubsystem.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Online/OnlineSessionNames.h"
+#include "OnlineSessionSettings.h"
+#include "Sessionsubsystem.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class GP3_UEFPS_API USessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Session")
+	void CreateLanSession(int32 PublicConnections = 3);
+
+
+private:
+    // Online Subsystem 取得
+    bool EnsureOnline();
+
+    // デリゲートハンドル管理（解除忘れ防止）
+    void ClearDelegates();
+
+    // Create の前に既存セッションがあれば壊す
+    void DestroyThenRecreate(int32 PublicConnections);
+
+    // Online インタフェース
+    IOnlineSubsystem* OSS = nullptr;
+    IOnlineSessionPtr  Session;
+
+    // デリゲート
+    FDelegateHandle OnCreateHandle;
+    FDelegateHandle OnDestroyHandle;
+
+    // コールバック
+    void OnCreateComplete(FName SessionName, bool bOk);
+
+};
+
+
